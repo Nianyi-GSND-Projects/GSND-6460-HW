1840721887344467e72dd44e5ac6bc9eb647f427
Nianyi Wang <wangnianyi2001@outlook.com>
2025-01-21 18:51:17 -0500

    Optimize the bundling process

--- S1 Directed Randomness/bundle.cjs
+++ S1 Directed Randomness/bundle.cjs
@@ -1,3 +1,11 @@
+/*
+ * Nianyi Wang's homework for GSND 6460: Generative Game Design - Sketch 1
+ * 
+ * This program is bundled from source code automatically.
+ * For more information about the source project, see the URL below.
+ * 
+ * https://github.com/Nianyi-GSND-Projects/GSND-6460-HW/blob/master/S1%20Directed%20Randomness/README.md
+ */
 var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
